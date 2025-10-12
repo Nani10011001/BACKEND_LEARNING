846c41022d236034d8a2e89142d60242b955d934
NANi <nani1531323@gmail.com>
2025-10-12 22:05:04 +0530

    Update index.js

--- NodeJS/Node js events/index.js
+++ NodeJS/Node js events/index.js
@@ -3,5 +3,5 @@ const eventEmitterInstance = new eventEmitter();
 eventEmitterInstance.once("responses",(name,id)=>{
 console.log(`user: ${name} id:${id}`)
 })
-eventEmitterInstance.emit("responses","nani","24781f0024")
+eventEmitterInstance.emit("responses","jai","24781f0024")
 eventEmitterInstance.emit("response","jhon","55")
